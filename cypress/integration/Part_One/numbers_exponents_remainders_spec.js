f55a4fc91ac5a41734b1320db957f3298058982d
“Noley <hollandnoley@gmail.com>
2021-04-09 11:55:19 -0700

    spelling

--- cypress/integration/Part_One/numbers_exponents_remainders_spec.js
+++ cypress/integration/Part_One/numbers_exponents_remainders_spec.js
@@ -4,8 +4,8 @@ describe("Exponents and operators testing", () => {
   it("testing the creating of the varaible sixSquared equal to 36", () => {
     expect(bundle.sixSquared).to.equal(36);
   });
-  it("testing the creating of the varaible fourCubed equal to 16", () => {
-    expect(bundle.fourCubed).to.equal(16);
+  it("testing the creating of the varaible fourCubed equal to 64", () => {
+    expect(bundle.fourCubed).to.equal(64);
   });
   it("testing the creating of the varaible sixtySixModSeven equal to 3", () => {
     expect(bundle.sixtySixModSeven).to.equal(3);
