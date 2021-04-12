3ba7e1964d660217c40d37bd81dc9f8552919453
“Noley <hollandnoley@gmail.com>
2021-04-12 15:40:42 -0700

    moreArrayTests

--- cypress/integration/Part_Two/arrays_for_loops_spec.js
+++ cypress/integration/Part_Two/arrays_for_loops_spec.js
@@ -4,4 +4,19 @@ describe("Arrays for loops testing", () => {
   it("testing the iteration of the nums array with a for loop, multiplying each element by 11", () => {
     expect(bundle.nums).to.eql([11, 22, 33, 44, 55, 66]);
   });
+  it("testing the iteration of weather array while pushing a concatenated string into weatherUpdate array", () => {
+    expect(bundle.weatherUpdate).to.eql([
+      "It is raining outside",
+      "It is sunny outside",
+      "It is stormy outside",
+      "It is hailing outside",
+    ]);
+  });
+  it("testing the iteration of 3 arrays while pushing a concatenated string into nationalParks array", () => {
+    expect(bundle.nationalParks).to.eql([
+      "Glacier National Park is in Montana and attractions are 700 miles of hiking trails",
+      "Yellowstone National Park is in Wyoming and attractions are hot springs and geysers",
+      "Yosemite National Park is in California and attractions are El Capitan and Half Dome",
+    ]);
+  });
 });
