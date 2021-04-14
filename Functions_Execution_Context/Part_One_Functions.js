983139a1c9dc00eee326b52c70326958d276a172
“Noley <hollandnoley@gmail.com>
2021-04-14 12:58:35 -0700

    functionsPartOne

--- Functions_Execution_Context/Part_One_Functions.js
+++ Functions_Execution_Context/Part_One_Functions.js
@@ -5,8 +5,8 @@
 const addTen = (num) => num + 10;
 
 // Uncomment below to check your work
-console.log(typeof addTen); // function
-console.log(addTen(5)); // 15
+// console.log(typeof addTen); // function
+// console.log(addTen(5)); // 15
 
 // Challenge continued
 
@@ -14,5 +14,48 @@ console.log(addTen(5)); // 15
 
 const addExclamation = (string) => (string += "!");
 
-console.log(typeof addExclamation); // function
-console.log(addExclamation("tacos")); // tacos!
+// Uncomment below to check your work
+// console.log(typeof addExclamation); // function
+// console.log(addExclamation("tacos")); // tacos!
+
+// Challenge continued
+
+// Create a function whatsUp that accepts a string, and returns the string "Whats up (string)?", with the passed in string added before the question mark.
+
+const whatsUp = (string) => `Whats up ${string}?`;
+
+// Uncomment below to check your work
+// console.log(typeof whatsUp); // function
+// console.log(whatsUp("doc")); // Whats up doc?
+
+// Challenge continued
+
+// Create a function addTwoNums that accepts two numbers as parameters, num1 and num2, and returns the string '(num1) plus (num2) equals (num1 + num2)'.
+
+const addTwoNums = (num1, num2) => `${num1} plus ${num2} equals ${num1 + num2}`;
+
+// Uncomment below to check your work
+// console.log(typeof addTwoNums); // function
+// console.log(addTwoNums(3, 4)); // 3 plus 4 equals 7
+
+// Challenge continued
+
+// Create a function lastLetter that utilizes bracket notation to return the last letter or character of a string.
+
+const lastLetter = (string) => string[string.length - 1];
+
+// Uncomment below to check your work
+// console.log(typeof lastLetter); // function
+// console.log(lastLetter("AVOCADOS")); // S
+// console.log(lastLetter("lime")); // e
+// console.log(lastLetter("salsa")); // a
+// console.log(lastLetter("guac!")); // !
+
+// Do not alter below this line
+export const bundle = {
+  addTen: addTen,
+  addExclamation: addExclamation,
+  whatsUp: whatsUp,
+  addTwoNums: addTwoNums,
+  lastLetter: lastLetter,
+};
