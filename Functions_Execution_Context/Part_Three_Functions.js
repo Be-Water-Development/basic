16429be844d1495592c113292a184e9949ed763e
“Noley <hollandnoley@gmail.com>
2021-04-15 15:29:40 -0700

    funcpartthree

--- Functions_Execution_Context/Part_Three_Functions.js
+++ Functions_Execution_Context/Part_Three_Functions.js
@@ -1,6 +1,6 @@
 // Challenge
 
-// Write a function sunshine that accepts the argument hour. Sunshine returns “Sun will rise soon” if the hour is before 6,
+// Create a function sunshine that accepts the argument hour. Sunshine returns “Sun will rise soon” if the hour is before 6,
 // “Sun is up” if the hour is before 19, and “Sun is setting” if the hour is 19 or after.
 
 const sunshine = (hour) => {
@@ -80,9 +80,78 @@ function treasure(array) {
 //   treasure(["sand", "seal", "dolphin", "seasquatch", "kelp", "urchins"]) // Keep searching!
 // );
 
+// Challenge continued
+
+// Complete the function endsWithS that accepts an array of strings, and iterates over the array using the build in forEach method.
+// Push any strings that end with the letter “s” into the results array, and return the updated results array.
+// You can learn about the forEach method here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+function endsWithS(array) {
+  let result = [];
+  //Add code here
+  array.forEach((ele) => {
+    if (ele.endsWith("s")) {
+      result.push(ele);
+    }
+  });
+  return result;
+}
+
+// Uncomment below to check your work
+// console.log(
+//   endsWithS([
+//     "lions",
+//     "lynx",
+//     "penguins",
+//     "tiger",
+//     "snakes",
+//     "otters",
+//     "meerkat",
+//   ]) //[ 'lions', 'penguins', 'snakes', 'otters' ]
+// );
+
+// Challenge continued
+
+// Complete the function y2K that accepts an array of numbers and iterates through the array. If the array contains the number 2000,
+// return the string “It’s the end of the world!”. Otherwise, return the string “World still exists”.
+// Do not use a variable to store the result that you are returning.
+
+function y2K(array) {
+  // Add code here
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 2000) {
+      return "It's the end of the world!";
+    }
+  }
+  return "World still exists";
+}
+
+// Uncomment below to check your work
+// console.log(y2K([1999, 3000, 2400, 4700, 1800, 7000])); // World still exists
+// console.log(y2K([1000, 2038, 2000, 1876, 1492, 1899])); // It's the end of the world!
+
+// Challenge continued
+
+// Using a for loop, create a function addX that accepts two parameters, array and x. AddX adds the argument x to each number in the array and returns the updated array.
+
+const addX = (array, x) => {
+  for (let i = 0; i < array.length; i++) {
+    array[i] += x;
+  }
+  return array;
+};
+
+// Uncomment below to check your work
+// console.log(typeof addX); // function
+// console.log(addX([1, 2, 3, 4], 2)); // [ 3, 4, 5, 6 ]
+// console.log(addX([7, 8, 9, 10], 10)); // [ 17, 18, 19, 20 ]
+
 // Do not alter below this line
 export const bundle = {
   sunshine: sunshine,
   getGrade: getGrade,
   treasure: treasure,
+  endsWithS: endsWithS,
+  y2K: y2K,
+  addX: addX,
 };
