c245d3a3b63beb74ab1a56c6fb0c39e81ea472b6
“Noley <hollandnoley@gmail.com>
2021-06-07 15:31:03 -0700

    readyForReview

--- cypress/integration/Async/async_part_two_spec.js
+++ cypress/integration/Async/async_part_two_spec.js
@@ -9,7 +9,7 @@ describe("testing forEach and timeLog functions", () => {
   });
   it("outputs are logged in correct order", () => {
     cy.spy(window.console, "log").as("consoleLog");
-    // How do you get cypress to care about order of console.logs...
+    // Xavyr: Can't get cypress to care about order of console logs, any ideas?
     cy.get("@consoleLog").should("have.callCount", 4);
     cy.get("@consoleLog").should("be.calledWith", "printing element 0");
     cy.get("@consoleLog").should("be.calledWith", "printing element 3");
